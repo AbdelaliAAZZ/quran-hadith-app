32761c80e6d85c1b9bf587451538a0a6aeaa3d5c
AbdelaliAAZZ <abdelaliaazzaoui@gmail.com>
2025-02-02 11:47:33 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         </Routes>
       </main>
       <footer className={`text-center py-12 ${theme === 'dark' ? 'bg-gray-900 text-gray-400' : 'bg-gray-100 text-gray-700'}`}>
-        <p className="text-sm">&copy; 2025 Quran & Hadith App. All rights reserved.</p>
+        <p className="text-sm">&copy; 2025 Quran & Hadith App. All rights reserved. محمد طه</p>
       </footer>
     </div>
   );
